b5fb007be32f7673f797a39524f742616d776f26
lost-in-noise <nancykikvadze@gmail.com>
2026-01-03 22:02:07 +0400

    create static methods in different classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,35 @@ package org.example;
 
 
 public class Main {
-    int x;
-    int y;
-    String name;
+//    int x;
+//    int y;
+//    String name;
 
     public static void main(String[] args) {
-        Main obj1 = new Main();
-        obj1.x = 3;
-        obj1.y = 4;
-        obj1.name = "John";
-        System.out.println(obj1.x + " " + obj1.y + " " + obj1.name);
-
-        Main obj2 = new Main();
-        obj2.x = 5;
-        System.out.println(obj2.x);
-
-        Main obj3 = new Main();
-        obj3.name = "Nino";
-        System.out.println(obj3.name);
+//        Main obj1 = new Main();
+//        obj1.x = 3;
+//        obj1.y = 4;
+//        obj1.name = "John";
+//        System.out.println(obj1.x + " " + obj1.y + " " + obj1.name);
+//
+//        Main obj2 = new Main();
+//        obj2.x = 5;
+//        System.out.println(obj2.x);
+//
+//        Main obj3 = new Main();
+//        obj3.name = "Nino";
+//        System.out.println(obj3.name);
+        printName();
+        Main object1 = new Main();
+        object1.printLastName();
     }
 
+    static void printName() {
+        System.out.println("my name is Nancy");
+    }
+
+    void printLastName() {
+        System.out.println("last name is Kikvadze");
+    }
 }
 
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        Main obj1 = new Main();
-        obj1.name = "Nancy";
-        System.out.println(obj1.name);
+//        Main obj1 = new Main();
+//        obj1.name = "Nancy";
+//        System.out.println(obj1.name);
+        Main.printName();
+        Main object1 = new Main();
+        object1.printLastName();
     }
 }
